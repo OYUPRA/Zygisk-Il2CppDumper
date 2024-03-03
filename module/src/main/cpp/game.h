7316b6843c517eef2e4dc38eb310afe1d2c04b64
OYUPRA <navariouz@gmail.com>
2024-03-03 14:59:34 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.uniu.dance.happy.id"
+#define GamePackageName "com.kakaogames.gdts"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
